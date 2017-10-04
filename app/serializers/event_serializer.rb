ea76647dfc1d2e6c594f8f02a6b6a97adedde0a9
slayTheSensei <kylejpearson@gmail.com>
2017-10-04 11:47:15 -0400

    EDITED CORS
    Allowed all origins to access api

--- app/serializers/event_serializer.rb
+++ app/serializers/event_serializer.rb
@@ -1,3 +1,3 @@
 class EventSerializer < ActiveModel::Serializer
-  attributes :id, :name, :venue, :long, :lat, :date, :user_events, :artist_events
+  attributes :id, :name, :venue, :long, :lat, :date, :artists, :artist_events
 end
--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -19,11 +19,11 @@ development_client_origin ||= ENV['CLIENT_ORIGIN']
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins do |origin, _env|
-      ENV['CLIENT_ORIGIN'] == '*' ||
-        origin == ENV['CLIENT_ORIGIN'] ||
-        origin == development_client_origin
-    end
+    origins '*'
+    #   ENV['CLIENT_ORIGIN'] == '*' ||
+    #     origin == ENV['CLIENT_ORIGIN'] ||
+    #     origin == development_client_origin
+    # endxs
     resource '*',
              headers: :any,
              methods: %i[options head get delete patch put post]
