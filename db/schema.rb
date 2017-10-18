b17991a371b5e3c85f6ac7a867641715faf2d736
slayTheSensei <kylejpearson@gmail.com>
2017-10-18 12:59:22 -0400

    ADDED TYPE TO EVENTS
    Created a type column to fix a previous oversight

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171018155936) do
+ActiveRecord::Schema.define(version: 20171018165756) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,6 +55,7 @@ ActiveRecord::Schema.define(version: 20171018155936) do
     t.datetime "date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "type"
   end
 
   create_table "examples", force: :cascade do |t|
